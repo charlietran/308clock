0351294b57b1624edf6943edafed71bb6496d839
Charlie Tran <hi@charlietran.com>
2013-06-24 20:33:07 -0400

    update.php

--- update.php
+++ update.php
@@ -9,31 +9,32 @@ $github_cidrs = array('204.232.175.64/27', '192.30.252.0/22');
 
 function cidr_match($ip, $ranges)
 {
-   $ranges = (array)$ranges;
-   foreach($ranges as $range) {
-       list($subnet, $mask) = explode('/', $range);
-       if((ip2long($ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet)) {
-           return true;
-       }
-   }
-   return false;
+  $ranges = (array)$ranges;
+  foreach($ranges as $range) {
+    list($subnet, $mask) = explode('/', $range);
+    if((ip2long($ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 try {
- $payload = json_decode($_POST['payload']);
+  $payload = json_decode($_POST['payload']);
 }
 catch(Exception $e) {
- file_put_contents('/home/charlie/charlietran.com/308clock/logs/github.txt', $e . ' ' . $payload, FILE_APPEND);
- exit(0);
+  file_put_contents('/home/charlie/charlietran.com/308clock/logs/github.txt', $e . ' ' . $payload, FILE_APPEND);
+  exit(0);
 }
 
 if ( $_POST['payload'] && (in_array($_SERVER['REMOTE_ADDR'], $github_ips) || cidr_match($_SERVER['REMOTE_ADDR'], $github_cidrs)) ) {
-    $output = shell_exec( 'cd /home/charlie/charlietran.com/308clock/ && git reset --hard HEAD 2>&1 && git pull 2>&1' );
-    file_put_contents('/home/charlie/charlietran.com/308clock/logs/github.txt', $output, FILE_APPEND);
+  $output = shell_exec( 'cd /home/charlie/charlietran.com/308clock/ && git reset --hard HEAD 2>&1 && git pull 2>&1' );
+  // file_put_contents('/home/charlie/charlietran.com/308clock/logs/github.txt', $output, FILE_APPEND);
 }
 
 else {
- header('HTTP/1.1 404 Not Found');
- echo '404 Not Found.';
- exit;
+  header('HTTP/1.1 404 Not Found');
+  echo '404 Not Found.';
+  exit;
 }
+
